d9ca79f3192c583960044b734a8efea36d4c2d9d
Tim Shuvalov <tashuvalov@gmail.com>
2025-11-09 20:59:27 +0300

    audio

--- Assets/Scripts/Infrastructure/GlobalAudioController.cs
+++ Assets/Scripts/Infrastructure/GlobalAudioController.cs
@@ -5,10 +5,14 @@ namespace Infrastructure
 {
     public class GlobalAudioController : MonoBehaviour
     {
+        public bool UseGlobalControl;
         public GlobalAudioSettings AudioSettings;
 
         private void FixedUpdate()
         {
+            if(!UseGlobalControl)
+                return;
+            
             AudioListener.pause = AudioSettings.Mute;
             AudioListener.volume = AudioSettings.Volume;
         }
